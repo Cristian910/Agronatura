ba5021cf27d310a744572d14a892fc96af285857
Cristian910 <ospinacristian0219@gmail.com>
2023-04-06 23:40:19 -0500

    proyecto

---
+++ js/menu.js
@@ -0,0 +1,33 @@
+const menu = document.querySelector(".menu");
+const openmenu = document.querySelector(".open-menu");
+const closemenu = document.querySelector(".close-menu");
+
+function togglemenu() {
+menu.classList.toggle("menu_opened");
+}
+openmenu.addEventListener("click", togglemenu);
+closemenu.addEventListener("click", togglemenu);
+
+$(document).ready(function() {
+$(openmenu).click(function() {
+$(".tit,section,footer,main").hide()   
+});
+})
+$(document).ready(function() {
+    $(closemenu).click(function() {
+    $(".tit,section,footer,main").show()   
+    });
+    })
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const elementosCarousel = document.querySelectorAll('.carousel');
+        M.Carousel.init(elementosCarousel, {
+            duration: 150,
+            dist: -80,
+            shift:5,
+            padding:5,
+            numVisible: 3,
+            indicators: true,
+            noWrap: false,
+        });
+        });
